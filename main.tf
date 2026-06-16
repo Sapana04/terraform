537fa614b5a355ac4d44659c7405f336e1ec0326
Hello <sapanasonavane12@gmail.com>
2026-06-16 14:43:03 +0530

    aad key

--- main.tf
+++ main.tf
@@ -94,6 +94,16 @@ data "aws_ami" "amazon_linux" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
+  resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+  
+  key_name      = "AWSnewaccount"
+
+  tags = {
+    Name = "Terraform-EC2-1"
+  }
+}
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web.id]
 
